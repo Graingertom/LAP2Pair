1961af8ecb4dbe5b45335be86d62ec7af06cd88b
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-03-23 12:21:45 +0000

    see previous commit message

--- Client/index.js
+++ Client/index.js
@@ -11,9 +11,9 @@ async function submitForm (e) {
 
     const postData = {
         title: e.target.children[0].innerText,
-        name: e.target.children[1].innerText,
+        pseudonym: e.target.children[1].innerText,
         content: e.target.children[2].innerText,
-        url: e.target.children[0].innerText + getDate
+        url: (e.target.children[0].innerText + getDate).split(" ").join("-").replace(/['"]+/g, '').toLowerCase()
     }
 
     console.log(postData)
@@ -29,7 +29,7 @@ async function submitForm (e) {
 
     console.log(options.body)
 
-    const response = await fetch('http://localhost:3000/posts', options);
+    const response = await fetch(`http://localhost:3000/posts/${options.body.url}`, options);
     const { url, err } = await response.json();
     if (err) {
         throw Error(err)
@@ -45,27 +45,42 @@ async function submitForm (e) {
 // Get content to load on url based on DB
 
 // onload event listener
+document.addEventListener("DOMContentLoaded", getItem)
 document.addEventListener("DOMContentLoaded", fill)
-
-function getData() {
-    try {
-        fetch('http://localhost:3000/posts')
-            .then((response) => {
-                return response.json()
-            })
-    } catch (err) {
-        console.log(err)
-    }
-}
-
-// if url === Homepage load HTML else hide form and display the data from DB based on the url
+let data
 
 async function getItem(url) {
+    let fullUrl = window.location.href
+    let splitUrl =  fullUrl.split('/')
+    let shortUrl = splitUrl[3]
     try {
-        const response = await fetch(`http://localhost:3000/${url}`);
+        const response = await fetch(`http://localhost:3000/${shortUrl}`);
         const data = await response.json();
         return data;
     } catch (err) {
         console.warn(err);
     }
 }
+
+function fill(data) {
+    if (window.location.href !== `http://127.0.0.1:5500/Client/index.html`) {
+        newPostForm.classList.add("hide");
+        const title = document.createElement("h1")
+        const pseudonym = document.createElement("p")
+        const content = document.createElement("p")
+
+        const titleText = document.createTextNode(`${data.title}`)
+        const pseudonymText = document.createTextNode(`${data.pseudonym}`)
+        const contentText = document.createTextNode(`${data.content}`)
+
+        body.appendChild(title)
+        body.appendChild(pseudonym)
+        body.appendChild(content)
+        title.appendChild(titleText)
+        pseudonym.appendChild(pseudonymText)
+        content.appendChild(contentText)
+    }
+}
+
+// if url === Homepage load HTML else hide form and display the data from DB based on the url
+

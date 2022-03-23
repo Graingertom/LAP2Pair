6305034e50c0b768679b67aa69ef4354ccf7d12d
Abdulrahman Awad <abdul.hmha@gmail.com>
2022-03-23 11:29:39 +0000

    Adding route and changing model

--- Server/Models/post.js
+++ Server/Models/post.js
@@ -1,8 +1,26 @@
+
 class Post {
     constructor(data){
-        this.is = data.id
+        this.id = data.id
         this.title = data.title
-        this.name = data.name
+        this.pseudonym = data.pseudonym
         this.content = data.content
     }
+
+    static async addPost(postData){
+        return new Promise (async(resolve,reject) => {
+            try{
+                let pd = postData.json
+                let blogData = db.query(`INSERT INTO blogs VALUES (id, title, pseudonym, content) VALUES (
+                    ${pd.id},${pd.name},
+                    ${pd.pseudonym},${pd.content}
+                ) RETURNING *;`)
+                let post = new Post(blogData.rows[0])
+                resolve(post)
+            } catch {
+                reject('Could not add blog post')
+            }
+        })
+    }
+
 }
---
+++ db/blogtable.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS blogs;
+
+CREATE TABLE blogs (
+    id serial PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    pseudonym VARCHAR(30) NOT NULL,
+    content VARCHAR(500) NOT NULL
+);
